3093c43bb89d7b6b5abb7abba9edc81b90b03593
jrmcauliffe00 <135724277+jrmcauliffe00@users.noreply.github.com>
2023-10-06 10:52:31 -0700

    Update docker_busybox_python.py
    update path

--- dags/docker_busybox_python.py
+++ dags/docker_busybox_python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from cwl_airflow.extensions.cwldag import CWLDAG
 dag = CWLDAG(
-    workflow="/Path/To/Workflow.cwl",
+    workflow="./dags/workflows/docker_busybox.cwl",
     dag_id="docker_busybox"
 )
