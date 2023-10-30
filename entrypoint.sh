6df9c2577606966140effbe369f8b5cad5ac0034
jrmcauliffe00 <135724277+jrmcauliffe00@users.noreply.github.com>
2023-10-30 13:19:33 -0400

    Update entrypoint.sh
    Remove comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Activate your virtual environment (if applicable)
-# source /path/to/your/virtualenv/bin/activate
-
 CWL_AIRFLOW_EXECUTABLE=/root/.local/bin/cwl-airflow
 # Run the cwl-airflow command
 "$CWL_AIRFLOW_EXECUTABLE" api "$@"
